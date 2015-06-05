959ebb60b6f8bffd3df45b741dcaa30444694fda
Rui Quelhas <rui@blissbooker.com>
2015-06-06 00:03:57 +0100

    Add basic functionality.

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # sdnv-stream
 
-## SDNV read/write stream
+Encode stream data as [SDNVs](http://www.dtnrg.org/wiki/SDNV).
+
+## Installation
+
+```
+$ npm install sdnv-stream
+```
+
+## Usage
+
+```javascript
+var sdnv = require('sdnv-stream');
+
+// Were, we use "through2" to setup the piping
+var through = require('through2');
+
+var stream = through();
+
+var encode = sdnv();
+// Let's set an encoding which allows the result to be displayed
+encode.setEncoding('hex');
+
+stream.pipe(encode).pipe(process.stdout);
+
+stream.write(new Buffer([0x7F])); // <Buffer 7f>
+stream.write(new Buffer([0x8F])); // <Buffer 81 0f>
+stream.write(new Buffer([0x12, 0x34])); // <Buffer a4 34>
+stream.write(new Buffer([0x0A, 0xBC])); // <Buffer 95 3c>
+```
+
--- test/index.js
+++ test/index.js
@@ -1,41 +1,55 @@
-var Lab = require('lab');
-var Stream = require('stream');
+'use strict';
 
-var sndv = require('../');
+var Lab = require('lab');
+var Code = require('code');
+
+var through = require('through2');
+var each = require('async-each');
+
+var sdnv = require('../');
 
 var lab = exports.lab = Lab.script();
 
 lab.experiment('sdnv-stream', function () {
-    var decoded, encoded;
 
-    lab.beforeEach(function (done) {
-        decoded = [
-            new Buffer([0x7F]),
-            new Buffer([0x8F]),
-            new Buffer([0x12, 0x34]),
-            new Buffer([0x0A, 0xBC])
-        ];
+    var decoded = [
+        new Buffer([0x7F]),
+        new Buffer([0x8F]),
+        new Buffer([0x12, 0x34]),
+        new Buffer([0x0A, 0xBC])
+    ];
 
-        encoded = [
-            new Buffer([0x7F]),
-            new Buffer([0x81, 0x0F]),
-            new Buffer([0xA4, 0x34]),
-            new Buffer([0x95, 0x3C])
-        ];
+    var encoded = [
+        new Buffer([0x7F]),
+        new Buffer([0x81, 0x0F]),
+        new Buffer([0xA4, 0x34]),
+        new Buffer([0x95, 0x3C])
+    ];
 
-        return done();
+    lab.test('should encode buffer', function (done) {
+
+        var stream = through();
+        var chunks = [];
+
+        var write = function (buffer, encoding, next) {
+
+            chunks.push(buffer);
+            return next();
+        };
+
+        var output = through(write);
+
+        stream.pipe(sdnv()).pipe(output);
+
+        var iterator = function (item, callback) {
+
+            return stream.write(item, 'hex', callback);
+        };
+
+        each(decoded, iterator, function (err) {
+
+            Code.expect(chunks).to.deep.equal(encoded);
+            return done();
+        });
     });
-
-    lab.test('should encode an input stream', function (done) {
-        return done();
-    });
-
-    lab.test('should decode an output stream', function (done) {
-        return done();
-    });
-
-    lab.test('should match output to input for codec pipe', function (done) {
-        return done();
-    });
-
 });
